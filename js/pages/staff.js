8181ef112b0beea73c831b1d90c0401077399d0e
Consencus <92981683+Consencus@users.noreply.github.com>
2026-01-10 18:20:24 -0500

    01/10/2026
    - Created staff.html
    - Created staff.css
    - Created staff.js
    
    Staff.html
    - Uses base HTML template from members.html
    
    Staff.css
    - Uses base CSS styling

---
+++ js/pages/staff.js
@@ -0,0 +1,7 @@
+/* ============================================
+   STAFF.JS
+   Staff page functionality
+   Coming soon in Phase 4!
+   ============================================ */
+
+console.log('📋 Staff page loaded - JavaScript coming soon!');
